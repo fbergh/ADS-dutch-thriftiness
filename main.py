64a2ccdc91fd859c322625058f67752ab0e186a1
fbergh <freekbergh@gmail.com>
2020-11-29 13:36:29 +0100

    Adapted test_server() to new algorithm

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import input_output as io
-from algorithm import NoDividers
+from algorithm import NoDividers, DumbSinglePass
 
 
 def test_server():
     n_products, n_dividers, costs = io.get_values_from_input()
-    algorithm = NoDividers()
-    algorithm.run(n_products, n_dividers, costs)
+    algorithm = DumbSinglePass()
+    min_sum = algorithm.run(n_products, n_dividers, costs)
+    io.eprint(min_sum)
+    io.send(min_sum)
 
 
 if __name__ == '__main__':
