c354f299149e96c30da1f36af2023765d4efd19c
fbergh <freekbergh@gmail.com>
2020-12-04 17:51:20 +0100

    Add function for testing locally

--- 0main.py
+++ 0main.py
@@ -1,14 +1,26 @@
 import input_output as io
-from algorithm import NoDividers, DumbSinglePass
+from algorithm import NoDividers, DumbSinglePass, BruteForce
+import random
 
 
 def test_server():
     n_products, n_dividers, costs = io.get_values_from_input()
-    algorithm = DumbSinglePass()
+    algorithm = BruteForce()
     min_sum = algorithm.run(n_products, n_dividers, costs)
     io.eprint(min_sum)
     io.send(min_sum)
 
 
+def test_local():
+    random.seed(1)
+    n_products = 100
+    n_dividers = random.randint(0, 25)
+    costs = [random.randint(1, 50) for _ in range(n_products)]
+    print(costs)
+    algorithm = BruteForce()
+    min_sum = algorithm.run(n_products, n_dividers, costs)
+    print(min_sum)
+
+
 if __name__ == '__main__':
-    test_server()
+    test_local()
