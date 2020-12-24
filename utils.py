ee72bfb2253651a059b26ffe124db18157dceb10
fbergh <freekbergh@gmail.com>
2020-12-24 09:05:27 +0100

    Rename value to gain and add function for getting mod 5 vales

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import math
 # Hard-coded dictionary for value of the last digit of a number modulo 5
 # E.g. 3 will be rounded to 5, so it costs 2 more. Therefore, ADDED_VALUE_DICT[3]=-2
 # -1 maps to -inf as a utility for the algorithm
-VALUE_DICT = {-1: -math.inf, 0: 0, 1: 1, 2: 2, 3: -2, 4: -1}
+GAIN_DICT = {-1: -math.inf, 0: 0, 1: 1, 2: 2, 3: -2, 4: -1}
 
 
 def round_to_5(x):
@@ -12,13 +12,21 @@ def round_to_5(x):
 
 
 def do_add_product(product, cur_cost):
-    return value(product + cur_cost) > value(cur_cost)
+    return gain(product + cur_cost) > gain(cur_cost)
 
 
-def value(cost):
+def gain(cost):
     # % 10, because we want last digit. % 5, because values in VALUE_DICT are the same after 4
-    return VALUE_DICT[cost % 10 % 5] if cost != -1 else VALUE_DICT[cost]
+    return GAIN_DICT[cost % 10 % 5] if cost != -1 else GAIN_DICT[cost]
 
 
 def cost_of_checkout(checkout):
-    return sum([round_to_5(cost) for cost in checkout])
+    return sum([round_to_5(cost)for cost in checkout])
+
+
+def get_mod_5_values(costs):
+    mod_5_values = []
+    for i in reversed(range(len(costs))):
+        if costs[i] % 5 == 0:
+            mod_5_values.append(costs.pop(i))
+    return mod_5_values
