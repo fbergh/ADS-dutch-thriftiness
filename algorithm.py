ac7c69be34a446ed6095637c8e0ccdd9b6443b7f
fbergh <freekbergh@gmail.com>
2020-12-24 11:18:25 +0100

    Add 'CheckoutCutting' algorithm

--- algorithm.py
+++ algorithm.py
@@ -12,45 +12,6 @@ class Algorithm(object):
         return None
 
 
-class DumbSinglePass(Algorithm):
-    """
-    Sequentially adds costs that do not round up (so ending on 0, 1, 2, 5, 6, 7) to one group
-    If a cost does round up (ending on 3, 4, 8, 9), place a divider and start the next product group
-    If there are no more dividers left, simply sum the remaining products
-    In the end returns sum of all product groups
-    """
-
-    def __init__(self):
-        super(DumbSinglePass, self).__init__()
-
-    def run(self, n_products, n_dividers, costs):
-        # Run base cases
-        base_cost = super().run(n_products, n_dividers, costs)
-        if base_cost is not None:
-            return base_cost
-
-        cost_of_group = -1
-        costs_of_groups = []
-        for i, c in enumerate(costs):
-            # If we have used all dividers, sum remaining products
-            if n_dividers == 0:
-                sum_remaining_prods = u.round_to_5(cost_of_group + sum(costs[i:]))
-                costs_of_groups.append(sum_remaining_prods)
-                break
-            # If adding a the product reduces our cost, add it
-            if u.do_add_product(c, cost_of_group):
-                if cost_of_group == -1:
-                    cost_of_group = 0
-                cost_of_group += c
-            # If not, place a divider and start again from the current product
-            else:
-                costs_of_groups.append(u.round_to_5(cost_of_group))
-                cost_of_group = c
-                n_dividers -= 1
-        # Return the sum of all groups of products, separated by dividers
-        return sum(costs_of_groups)
-
-
 class BruteForce(Algorithm):
     """
     Brute force algorithm that tries all possible combinations given the number of dividers
@@ -64,9 +25,9 @@ class BruteForce(Algorithm):
     def run(self, n_products, n_dividers, costs):
         print("Brute force")
         # Run base cases
-        base_cost = super().run(n_products, n_dividers, costs)
-        if base_cost is not None:
-            return base_cost
+        base_case_cost = super().run(n_products, n_dividers, costs)
+        if base_case_cost is not None:
+            return base_case_cost
         else:
             # Set initial minimum to checkout with no dividers
             self.min_checkout = u.round_to_5(sum(costs))
@@ -128,3 +89,87 @@ class BruteForce(Algorithm):
             all_checkouts.extend(children_append)
 
             return all_checkouts
+
+    def reset(self):
+        self.min_checkout = math.inf
+
+
+class CheckoutCutting(Algorithm):
+    """
+    This algorithm is inspired by the rod cutting algorithm. We look at all possible positions to place a divider.
+    On the most optimal place, if it exists, we place one and then repeat the same procedure for the worst side.
+    If it does not exist, we are done with that side.
+
+    NOTE: the algorithm is only partially correct now (seems to be about 80-90% correct)
+
+    Algorithm is O(n*k) (I think), where n=#products and k=#dividers
+    """
+    def __init__(self):
+        super(CheckoutCutting, self).__init__()
+
+    def run(self, n_products, n_dividers, costs):
+        # Run base cases
+        base_case_cost = super().run(n_products, n_dividers, costs)
+        if base_case_cost is not None:
+            return base_case_cost
+        else:
+            return self.checkout_cutting(n_dividers, costs)
+
+    def checkout_cutting(self, n_dividers, costs):
+        print(costs)
+        if len(costs) == 1 or n_dividers == 0:
+            return u.cost_of_checkout([costs])
+        else:
+            # Set best_checkout to checkout with no dividers to determine if it's even worth placing dividers
+            best_checkout = [costs]
+            # Bad initial values for gain
+            best_gain = (-math.inf, -math.inf)
+            # For every position where we can place the divider (so not 0)
+            for div_pos in range(1, len(costs)):
+                cur_checkout = [u.to_list(costs[:div_pos]), u.to_list(costs[div_pos:])]
+                cur_gain = u.gain_of_checkout(cur_checkout)
+                if self.is_cur_checkout_better(best_checkout, best_gain, cur_checkout, cur_gain):
+                    best_checkout = cur_checkout
+                    best_gain = cur_gain
+                if best_gain == (2, 2) and n_dividers == 1:
+                    return u.cost_of_checkout(best_checkout)
+
+            # If we have used no divider, stop recursing and return sum
+            if best_gain == (-math.inf, -math.inf):
+                return u.cost_of_checkout(best_checkout)
+            # If we have used one, keep best side in tact and recurse
+            else:
+                best_idx = self.get_idx_best_side(best_gain, best_checkout)
+                best_side, worst_side = best_checkout[best_idx], best_checkout[not best_idx]
+                # NOTE: only recurse on bad side? Probably not...
+                # If we want to recurse on best side too, dividers should be a class attribute, but how do we decide
+                # which side gets to use how many dividers?
+                return u.round_to_5(sum(best_side)) + self.checkout_cutting(n_dividers - 1, worst_side)
+
+    def is_cur_checkout_better(self, best_checkout, best_gain, cur_checkout, cur_gain):
+        # NOTE: the algorithm is partially correct, some of these cases might therefore be incorrect...
+
+        # If the current cost is greater than the best checkout, it sucks
+        if u.cost_of_checkout(cur_checkout) > u.cost_of_checkout(best_checkout):
+            return False
+        # If the current cost is smaller than the best checkout, it's great
+        elif u.cost_of_checkout(cur_checkout) < u.cost_of_checkout(best_checkout):
+            return True
+        # If the maximal gain value of the current checkout is better than that of the best checkout, it's great
+        # E.g. given current (2,-2) and best (1,-1), then 2 > 1, so current > best
+        if max(cur_gain) > max(best_gain):
+            return True
+        # If the gain values of best and current are equal, we check if the same gain is achieved with fewer values in current
+        # If so, it's great
+        is_gain_equal = sorted(best_gain) == sorted(cur_gain)
+        if is_gain_equal:
+            max_idx_c = cur_gain.index(max(cur_gain))
+            max_idx_b = best_gain.index(max(best_gain))
+            return len(best_checkout[max_idx_b]) > len(cur_checkout[max_idx_c])
+        # If none of the above hold, it sucks
+        return False
+
+    def get_idx_best_side(self, best_gain, best_checkout):
+        # If index 1 is the best this will return True (i.e. 1), else False (i.e. 0)
+        return best_gain[1] > best_gain[0] \
+               or best_gain[1] == best_gain[0] and len(best_checkout[1]) < len(best_checkout[0])
