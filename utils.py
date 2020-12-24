ec7c430787803dfea47ca569bd096ad747e8a8c0
fbergh <freekbergh@gmail.com>
2020-12-24 11:23:34 +0100

    Add documentation and remove irrelevant code from utils

--- utils.py
+++ utils.py
@@ -3,21 +3,16 @@ import math
 
 # Hard-coded dictionary for value of the last digit of a number modulo 5
 # E.g. 3 will be rounded to 5, so it costs 2 more. Therefore, ADDED_VALUE_DICT[3]=-2
-# -1 maps to -inf as a utility for the algorithm
-GAIN_DICT = {-1: -math.inf, 0: 0, 1: 1, 2: 2, 3: -2, 4: -1}
+GAIN_DICT = {0: 0, 1: 1, 2: 2, 3: -2, 4: -1}
 
 
 def round_to_5(x):
     return 5 * round(x/5)
 
 
-def do_add_product(product, cur_cost):
-    return gain(product + cur_cost) > gain(cur_cost)
-
-
 def gain(cost):
     # % 10, because we want last digit. % 5, because values in VALUE_DICT are the same after 4
-    return GAIN_DICT[cost % 10 % 5] if cost != -1 else GAIN_DICT[cost]
+    return GAIN_DICT[cost % 10 % 5]
 
 
 def cost_of_checkout(checkout):
@@ -28,6 +23,7 @@ def cost_of_checkout(checkout):
 
 
 def gain_of_checkout(checkout_with_divider):
+    """ Computes the gain of the left and right side of a checkout with one divider """
     left, right = checkout_with_divider
     if type(left) == int:
         left = [left]
@@ -37,6 +33,7 @@ def gain_of_checkout(checkout_with_divider):
 
 
 def get_mod_5_values(costs):
+    """ Gets all values x, for which holds x % 5 = 0, and *removes* them from the costs list"""
     mod_5_values = []
     for i in reversed(range(len(costs))):
         if costs[i] % 5 == 0:
