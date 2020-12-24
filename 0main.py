5d268f071562e24f7458930c1396c76904beb759
fbergh <freekbergh@gmail.com>
2020-12-24 11:13:46 +0100

    Minor improvements to printing and seed

--- 0main.py
+++ 0main.py
@@ -1,6 +1,6 @@
 import input_output as io
 from problem_io import *
-from algorithm import BruteForce
+from algorithm import BruteForce, CheckoutCutting
 import random
 
 
@@ -12,26 +12,33 @@ def test_server():
     io.send(min_sum)
 
 
-def test_local(algorithm, n_problems, seed=None):
+def test_local(algorithm, n_problems, seed=None, do_sample_problems=False):
     brute = BruteForce()
 
-    print("Sample problem 1")
-    n_products, n_dividers, costs = data_sample1()
-    print(algorithm.run(n_products, n_dividers, costs.copy()) == 755, "\n")
+    if do_sample_problems:
+        print("Sample problem 1")
+        n_products, n_dividers, costs = data_sample1()
+        print(algorithm.run(n_products, n_dividers, costs.copy()) == 755, "\n")
 
-    print("Sample problem 2")
-    n_products, n_dividers, costs = data_sample2()
-    print(algorithm.run(n_products, n_dividers, costs.copy()) == 0, "\n")
+        print("Sample problem 2")
+        n_products, n_dividers, costs = data_sample2()
+        print(algorithm.run(n_products, n_dividers, costs.copy()) == 0, "\n")
 
     if seed is not None:
+        random.seed(seed)
+    else:
+        seed = random.random()
+        print(seed)
         random.seed(seed)
 
     correct = []
 
     for i in range(n_problems):
         print(f"--- Problem {i+1} ---")
-        n_products, n_dividers, costs = data_random(10)
+        n_products, n_dividers, costs = data_random(20)
+        print(n_products, n_dividers, costs, "\n", sep="\n")
         correct_answer = brute.run(n_products, n_dividers, costs.copy())
+        brute.reset()
         print()
         answer = algorithm.run(n_products, n_dividers, costs.copy())
         correct.append(correct_answer == answer)
@@ -41,5 +48,5 @@ def test_local(algorithm, n_problems, seed=None):
 
 
 if __name__ == '__main__':
-    a = BruteForce()
-    test_local(a, 10)
+    a = CheckoutCutting()
+    test_local(a, 10)#, 0.39701436528658685)
